5551a823a46467f2c66e7ac255725a16175a6f02
AbdurRakib960 <rakibabdur960git@gmail.com>
2021-03-21 04:31:38 +0600

    component - simillar in data different in look practice

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const design = {
+    backgroundColor: 'red',
+    color: 'yellow',
+    padding:'10px'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h2>React practice</h2>
+       <Person name='Rakib Talukdar' mobile= '23434'></Person>
+       <Person name='Shakib hasan' mobile='53433'></Person>
+       <Person name="Rubel hussain" mobile='34565354'></Person>
+       <Person name="Tomas" mobile='34553432'></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  const personStyle = {
+    backgroundColor: 'orange',
+    color: 'black',
+    margin: '10px',
+    padding: '5px',
+    borderBottom: '1px solid white'
+  }
+  return (
+    <div style = {personStyle}>
+      <h3>Name : {props.name}</h3>
+      <h4>mobile : {props.mobile}</h4>
+    </div>
+  )
 }
 
 export default App;
